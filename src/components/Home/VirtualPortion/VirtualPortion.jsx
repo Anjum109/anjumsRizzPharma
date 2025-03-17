d115ec59b76e7cb635c905e3b6e3584106f3bcc1
Anjum109 <aunjumtuli109@gmail.com>
2025-03-17 16:29:39 +0600

    footer

--- src/components/Home/VirtualPortion/VirtualPortion.jsx
+++ src/components/Home/VirtualPortion/VirtualPortion.jsx
@@ -5,22 +5,22 @@ export default function VirtualPortion() {
     return (
         <div className='mt-20 '>
             <div className='lg:flex justify-between px-5 lg:px-[135px] py-[29px] absolute w-full'>
-                <div className='text-white text-[38px] lg:text-[58px] impact-font'>
+                <div className='text-white text-[28px] lg:text-[58px] impact-font'>
                     <p>100% Online,
                     </p>
                     <p>100% Convenient</p>
                 </div>
-                <div className='text-[14px] mt-2 lg:mt-0 font-normal text-white' style={{ fontFamily: 'Neue Montreal' }}>
+                <div className=' text-[10px] lg:text-[14px] mt-2 lg:mt-0 font-normal text-white' style={{ fontFamily: 'Neue Montreal' }}>
                     <div className='flex gap-3 items-center '>
-                        <img src={arrow} alt='arrow' />
+                        <img src={arrow} alt='arrow' className='lg:w-[18px] w-[10px] lg:h-[19px] h-[10px]' />
                         <p>Chat with a provider 24/7</p>
                     </div>
                     <div className='flex gap-3 items-center '>
-                        <img src={arrow} alt='arrow' />
+                        <img src={arrow} alt='arrow' className='lg:w-[18px] w-[10px] lg:h-[19px] h-[10px]' />
                         <p>Fast, discreet shipping</p>
                     </div>
                     <div className='flex gap-3 items-center '>
-                        <img src={arrow} alt='arrow' />
+                        <img src={arrow} alt='arrow' className='lg:w-[18px] w-[10px] lg:h-[19px] h-[10px]' />
                         <p>Clinically proven ingredients and FDA-approved treatments.</p>
                     </div>
                 </div>
